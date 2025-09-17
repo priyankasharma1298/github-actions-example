63212f1f9b8ca4a4a089bc84533f50d9ff68c007
Priyankasharma1298 <priyankasharmapce@gmail.com>
2025-09-17 14:28:26 +0530

    first commit

---
+++ src/main/java/cicd/Controller/TestController.java
@@ -0,0 +1,14 @@
+package cicd.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    // Simple endpoint to check app health
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot on EC2!";
+    }
+}
